1542b4eaf99994da9257d5ea821b970758030b25
Ignacio Chiaravalle <nachochiara@gmail.com>
2020-04-17 17:00:51 -0300

    Arreglo conflicto. Falta Fabio

--- Pruebas Repositorio/src/Main.java
+++ Pruebas Repositorio/src/Main.java
@@ -2,15 +2,7 @@
 public class Main {
 	
 	public static void main(String[] args) {
-		
-<<<<<<< HEAD
-		System.out.println("Probando Fernandez Felipe e Ignacio Chiaravalle. ...");
-=======
-
 		System.out.println("Probando Ignacio Chiaravalle, Joaquin Rau y Maximiliano Montenegro, Fernandez Felipe.");
-
->>>>>>> 4baf720ec1a926d502e421e6928bbd895ad7635c
-		
 	}
 
 }
