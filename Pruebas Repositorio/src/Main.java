e6980bf74dc573c38df96b70d95fa849c7556fd9
Maxi0512 <mampuyol512@gmail.com>
2020-04-17 16:48:24 -0300

    commit nombre maxi

--- Pruebas Repositorio/src/Main.java
+++ Pruebas Repositorio/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("Probando Fernandez Felipe e Ignacio Chiaravalle.");
+		System.out.println("Probando Fernandez Felipe, Ignacio Chiaravalle y Maximiliano Montenegro.");
 		
 	}
 
